687be6c2018d8b2e6b304463f1a1bcbdd933994a
Michael-Kochis <rhishisikk@gmail.com>
2021-06-28 10:35:06 -0500

    Working on internal database, no persistance.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+const PORT = 5000;
+
+const fakeIPSource = [];
+
+const sayHello = () => {
+    console.log(`server is listening on port: ${PORT}`);
+}
+
+server.get("/", (req,res) => {
+    res.json({message: "Yip, yip, Appa!"});
+})
+
+server.get("/api/ipsource/:id", (req, res) => {
+    const {id} = req.params;
+    let unit = fakeIPSource.find(record => record.sourceID === id);
+    res.status(200).json(id);
+})
+
+server.get("/api/ipsource", (req, res) => {
+    res.status(201).json(fakeIPSource);
+})
+
+server.post("/api/ipsource", (req, res) => {
+    let neoSource = {
+         sourcename: req.body.sourcename,
+         sourceID: fakeIPSource.length 
+    }
+    fakeIPSource.push(neoSource);
+    res.status(201).json(neoSource);
+})
+
+server.listen(PORT, sayHello);

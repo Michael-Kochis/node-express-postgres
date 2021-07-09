81cbf1188f1b1a62b0abc7765d330b0e2017c5e9
Michael-Kochis <rhishisikk@gmail.com>
2021-07-09 15:10:00 -0500

    Trial of internal logger

---
+++ api/logger/logger.js
@@ -0,0 +1,28 @@
+const db = require('../data/dbConfig');
+
+function logger(req, res, next) {
+    //request method, request url, and a timestamp
+    const method = req.method;
+    const url = req.url;
+    const time = Date.now();
+
+    const decoded = req.decoded.id || 0;
+
+    const neoLogMessage = {
+        id: Date.now(),
+        who: decoded,
+        when: time,
+        what: method,
+        where: url
+    }
+
+    return db('logs')
+    .insert(neoLogMessage)
+    .then(() => {
+      next();
+    }).catch(next);
+}
+  
+module.exports = {
+    logger
+}  
--- api/server.js
+++ api/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const authRouter = require('./auth/auth-routes')
 const ipSourceRouter = require('../routes/ipsource-routes');
 const userRouter = require('../routes/user-routes');
 
+const { logger } = require('./logger/logger');
+const { verifyToken } = require('./auth/auth-middleware');
+
 const server = express();
+server.use(helmet() );
 server.use(express.json() );
 server.use(cors() );
 
+server.use(logger() );
+
 server.use("/api/auth", authRouter);
-server.use("/api/ipsource", ipSourceRouter);
-server.use("/api/users", userRouter);
+server.use("/api/ipsource", verifyToken, ipSourceRouter);
+server.use("/api/users", [verifyToken], userRouter);
 
 server.get("/", (req,res) => {
     res.json({message: "Yip, yip, Appa!"});
